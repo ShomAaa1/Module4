6727dfc5865d6ee7780bd7f0ff1b1079081a750a
Shoma10 <shomar10@rambler.ru>
2025-08-11 19:48:43 +0300

    add selection sort to lesson19

---
+++ Lesson19/SelectionSort.py
@@ -0,0 +1,16 @@
+def selection_sort(arr: list[int]) -> list[int]:
+    n = len(arr)
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+# Пример использования:
+my_list = [64, 34, 25, 12, 22, 11, 90]
+selection_sort(my_list)
+print("Отсортированный список:", my_list)
